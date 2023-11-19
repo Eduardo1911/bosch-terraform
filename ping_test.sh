67530e2747852a268ee39a6b139c1a3a096f8ad8
Eduard Mihail <ext-eduard-mihail.costoae@fiskars.com>
2023-11-19 15:16:55 +0200

    add aws region to ping command

--- ping_test.sh
+++ ping_test.sh
@@ -5,7 +5,7 @@ VM_IDS=("${@:2}")
 SOURCE=${VM_IDS[0]}
 
 for DESTINATION in "${VM_IDS[@]}"; do
-  ping -c 1 -W 1 $(aws ec2 describe-instances --instance-ids $DESTINATION --query "Reservations[0].Instances[0].PublicIpAddress" --output text) > /dev/null 2>&1
+  ping -c 1 -W 1 $(aws ec2 describe-instances --region eu-central-1 --instance-ids $DESTINATION --query "Reservations[0].Instances[0].PublicIpAddress" --output text) > /dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "Ping from $SOURCE to $DESTINATION: PASS"
   else
