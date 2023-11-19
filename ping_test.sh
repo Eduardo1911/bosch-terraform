3973d002e6dbd8799259a30952400161a62ca60b
Eduard Mihail <ext-eduard-mihail.costoae@fiskars.com>
2023-11-19 16:21:33 +0200

    tweak ping script

--- ping_test.sh
+++ ping_test.sh
@@ -6,9 +6,9 @@ VM_IPS="${@:2}"
 
 # Ping Test
 for ((i=0; i<$VM_COUNT; i++)); do
-  SOURCE_IP=$(echo $VM_IPS | cut -d ' ' -f $((i+1)))
+  SOURCE_IP=$(echo $VM_IPS | cut -d ' ' -f $i)
   DESTINATION_INDEX=$(( (i+1) % VM_COUNT ))
-  DESTINATION_IP=$(echo $VM_IPS | cut -d ' ' -f $((DESTINATION_INDEX + 1)))
+  DESTINATION_IP=$(echo $VM_IPS | cut -d ' ' -f $DESTINATION_INDEX)
 
   # Run ping and capture result
   ping_result=$(ping -c 1 $DESTINATION_IP > /dev/null 2>&1 && echo "pass" || echo "fail")
