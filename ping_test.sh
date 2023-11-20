6d4a9b4dcd698d19369e7f5c0614865f6d04144e
Eduard Mihail <ext-eduard-mihail.costoae@fiskars.com>
2023-11-20 10:18:30 +0200

    change ping test

--- ping_test.sh
+++ ping_test.sh
@@ -9,14 +9,14 @@ CURRENT_VM_INDEX=$(hostname | grep -o '[0-9]*')
 
 # Ping Test
 for ((i=0; i<$VM_COUNT; i++)); do
-  SOURCE_IP=$(echo $VM_IPS | cut -d ' ' -f $i)
+  SOURCE_IP=$(echo $VM_IPS | cut -d ' ' -f $((i+1)))
   DESTINATION_INDEX=$(( (i+1) % VM_COUNT ))
-  DESTINATION_IP=$(echo $VM_IPS | cut -d ' ' -f $DESTINATION_INDEX)
+  DESTINATION_IP=$(echo $VM_IPS | cut -d ' ' -f $((DESTINATION_INDEX + 1)))
 
   # Run ping and capture result
   ping_result=$(ping -c 1 $DESTINATION_IP > /dev/null 2>&1 && echo "pass" || echo "fail")
 
-  # Output the ping result only for the current VM
+  # Output the ping result
   if [ $i -eq $CURRENT_VM_INDEX ]; then
     echo "Ping from $SOURCE_IP to $DESTINATION_IP: $ping_result" > /tmp/ping_results.txt
   fi
